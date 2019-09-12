31b9ca55fd7fc859cc2873c31f8043186a19804d
tuipik <tuipiku@gmail.com>
2019-09-12 13:42:49 +0300

    added handler

---
+++ config.py
@@ -0,0 +1,30 @@
+
+
+
+images = [
+    '.jpg',
+    '.jpeg',
+    # '.bmp',
+    # '.png',
+]
+
+del_original_image_after_converting = True
+
+documents = [
+    '.doc',
+    '.docx',
+    # '.xls',
+    # '.xlsx',
+    # '.ods',
+]
+del_original_document_after_converting = True
+
+teamviewer_cache = [
+    '.qmlc'
+]
+
+
+files_to_move = [
+    '.log'
+]
+path_to_move = ''
